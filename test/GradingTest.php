2fef0427b09700cda244f855d4b861cd5961ccb9
Sitdhibong Laokok <sitdhibong@gmail.com>
2017-09-21 09:16:33 +0700

    Add default Test case

---
+++ test/GradingTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use Grading\Grading;
+
+class GradingTest extends TestCase {
+
+  public function testCanPerformUnitTest(): void {
+    $this->assertTrue(true);
+  }
+}
